1c274cedfbba432139531b95bf6729651201560f
wend3ll-souza <wend3llsouza@gmail.com>
2021-04-11 22:10:13 -0300

    :package: initial structure setDataInLocalStorage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import data from '../../data';
 
 function Home() {
+
+  const setDataInLocalStorage = () => {
+    localStorage.setItem('dataCards', JSON.stringify(data));
+  };
+
+  useEffect(() => {
+    setDataInLocalStorage()
+  }, []);
+
   return(
     <>
     <h1>Home</h1>
+    <section>
+     <img src="" alt=""/>
+      <p></p>
+    </section>
     </>
   );
 }
